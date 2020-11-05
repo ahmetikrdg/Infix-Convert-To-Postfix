fe828f8c6366b12900249b7eb8de733a47cf7a22
Your Name <you@example.com>
2020-11-05 09:05:20 +0300

    first commit

---
+++ Infix convert to postfix/InfixApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infix_convert_to_postfix
+{
+    class InfixApp
+    {
+        static void Main(string[] args)
+        {
+            String input, output;
+            while (true)
+            {
+                Console.Write("Enter infix: ");
+                //System.out.flush();
+                input= Console.ReadLine(); // read a string from kbd
+                if (input.Equals("")) // quit if [Enter]
+                    break;
+                // make a translator
+                InToPost theTrans = new InToPost(input);
+                output = theTrans.doTrans(); // do the translation
+                Console.WriteLine("Postfix is " + output + '\n');
+            } // end while
+        } // en
+    }
+}
---
+++ Infix convert to postfix/StackX.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infix_convert_to_postfix
+{
+    public class StackX
+    {
+        private int maxSize;
+        private char[] stackArray;
+        private int top;
+        //-------------------------------------------------------------
+        
+         public StackX(int s) // constructor
+                {
+                    maxSize = s;
+                    stackArray = new char[maxSize];
+                    top = -1;
+                }
+        //-------------------------------------------------------------
+        
+         public void push(char j) // put item on top of stack
+                { stackArray[++top] = j; }
+        //-------------------------------------------------------------
+        
+         public char pop() // take item from top of stack
+                { return stackArray[top--]; }
+        //-------------------------------------------------------------
+        
+         public char peek() // peek at top of stack
+                { return stackArray[top]; }
+        //-------------------------------------------------------------
+        
+         public Boolean isEmpty() // true if stack is empty
+                { return (top == -1); }
+                //-------------------------------------------------------------
+                public int size() // return size
+                { return top + 1; }
+        //-------------------------------------------------------------
+        
+         public char peekN(int n) // return item at index n
+                { return stackArray[n]; }
+        //-------------------------------------------------------------
+        
+         public void displayStack(String s)
+         {
+            Console.Write(s);
+            Console.Write("Stack (bottom-->top): ");
+            for (int j = 0; j < size(); j++)
+            {
+                Console.Write(peekN(j));
+                Console.Write(' ');
+            }
+            Console.WriteLine("");
+         }
+    }
+}
